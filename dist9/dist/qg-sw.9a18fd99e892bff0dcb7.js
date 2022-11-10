1b9f7dd87a55cb022be3b23d6ef81455492ac1d5
Avijit Naskar <avijit.naskar@zee.com>
2022-11-10 17:47:43 +0530

    update10

---
+++ dist9/dist/qg-sw.9a18fd99e892bff0dcb7.js
@@ -0,0 +1,2 @@
+var appId = "9a18fd99e892bff0dcb7";
+importScripts("https://cdn.qgraph.io/v3/r/qg-sw.js");
---
+++ dist9/dist/qg-sw.ae5ecb63a30974627faa.js
@@ -0,0 +1,2 @@
+var appId = "ae5ecb63a30974627faa";
+importScripts("https://cdn.qgraph.io/v3/r/qg-sw.js");
